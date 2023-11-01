65cbd9fe45bccc1d32dc925ddae7431d05c2b596
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2023-11-01 12:48:14 +0530

    Initial Commit

---
+++ YouCrypto/YouCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouCryptoApp.swift
+//  YouCrypto
+//
+//  Created by Harsh Chaturvedi on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct YouCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
